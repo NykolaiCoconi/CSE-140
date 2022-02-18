ed45e9283d06bbded6c7ad1f2dd9c4c3e93f0503
NykolaiCoconi <93291345+NykolaiCoconi@users.noreply.github.com>
2022-02-17 16:59:22 -0800

    the start

--- Lab3/MachineCodeDecoder.py
+++ Lab3/MachineCodeDecoder.py
@@ -1,1 +1,19 @@
 #Made By: Nykolai Coconi & 
+#ask for the input, split the input into an array, check the first 6 digits for opcode, 
+#depending on type of instruction, go to a section to decode what the other elements equal. 
+#Translate opcode to hex and then check against the registry they gave us
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Placeholder
